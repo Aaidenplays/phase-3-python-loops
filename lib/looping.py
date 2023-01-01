15a6473f9ce0bd51443d182031cd966aa5b5e81c
Aaiden Witten <aaidenplays@yahoo.com>
2023-01-01 16:09:07 -0600

    super fun lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
+import ipdb
+
 def happy_new_year():
-    # code goes here
+    i = 10
+    while i >= 0:
+        if(i > 0):
+            print(i)
+        else:
+            print('Happy New Year!')
+        i-=1
     pass
 
-def square_integers(int_list):
-    # code goes here
-    pass
+def square_integers(int_list): return [num**2 for num in int_list]
+
 
 def fizzbuzz():
-    # code goes here
-    pass
+    def is_fizzbuzz(num):
+        if(num % 3 == 0 and num % 5 == 0):
+            return "FizzBuzz"
+        if(num % 3 == 0):
+            return "Fizz"
+        if(num % 5 == 0):
+            return "Buzz"
+        else:
+            return num
+    return [print(is_fizzbuzz(num+1)) for num in range(100)]
